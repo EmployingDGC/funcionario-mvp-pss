a6d4100adac03e7bedb357584f0479ee77874612
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-10-30 00:12:18 -0300

    feat: campo cargo

--- src/main/java/com/pss/model/Funcionario.java
+++ src/main/java/com/pss/model/Funcionario.java
@@ -1,15 +1,30 @@
 package com.pss.model;
 
 public class Funcionario extends Usuario {
+    private String cargo;
     private double salario;
 
-    public Funcionario(String nome, String login, String senha, double salario) {
+    public Funcionario(String nome, String login, String senha, String cargo, double salario) {
         super(nome, login, senha);
 
         this.salario = salario;
+        this.cargo = cargo.trim().toLowerCase();
     }
 
     public double getSalario() {
         return this.salario;
+    }
+
+    public String getCargo() {
+        return this.cargo;
+    }
+
+    @Override
+    public String toString() {
+        return (
+            super.toString() + "\n"
+            + "Cargo: " + this.cargo + "\n"
+            + "Salário: " + Double.toString(this.salario)
+        );
     }
 }
