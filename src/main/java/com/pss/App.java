dbd1fc567a220e3158bc59343ef3ef76c6821c17
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-11-01 19:50:11 -0300

    fix: removendo valores de exemplo da aplicação

--- src/main/java/com/pss/App.java
+++ src/main/java/com/pss/App.java
@@ -1,7 +1,5 @@
 package com.pss;
 
-import com.pss.collection.FuncionarioCollection;
-import com.pss.model.Funcionario;
 import com.pss.presenter.PrincipalPresenter;
 
 /**
@@ -13,38 +11,6 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        FuncionarioCollection funcionarios = FuncionarioCollection.getInstancia();
-
-        funcionarios.add(
-            new Funcionario(
-                "Davi Guizani",
-                "dguizani",
-                "12345678",
-                "Analista de Dados",
-                1000.0
-            )
-        );
-
-        funcionarios.add(
-            new Funcionario(
-                "Jeniffer",
-                "koi",
-                "12345678",
-                "Streamer",
-                1000.0
-            )
-        );
-
-        funcionarios.add(
-            new Funcionario(
-                "Guilherme",
-                "gui",
-                "12345678",
-                "Analista de Negócios",
-                1000.0
-            )
-        );
-
         PrincipalPresenter principalView = new PrincipalPresenter();
         principalView.getJanela().setVisible(true);
     }
