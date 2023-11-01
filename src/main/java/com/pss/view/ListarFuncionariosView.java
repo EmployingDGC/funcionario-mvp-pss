080b7791906af91f8f4c531bca72accb95e1c854
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2023-11-01 19:46:50 -0300

    feat: adicionando campo e botão de pesquisa

--- src/main/java/com/pss/view/ListarFuncionariosView.java
+++ src/main/java/com/pss/view/ListarFuncionariosView.java
@@ -4,23 +4,37 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+
 import javax.swing.table.TableColumnModel;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Dimension;
 
 public class ListarFuncionariosView extends BaseView {
     private JTable tabela;
     private DefaultTableModel tabelaModel;
     private TableColumnModel colunasModel;
 
+    private JTextField campoPesquisa;
+
     private JButton botaoEditar;
     private JButton botaoVisualizar;
     private JButton botaoExcluir;
+    private JButton botaoPesquisar;
 
     public ListarFuncionariosView() {
         super(new BorderLayout());
+
+        JPanel painelPesquisa = new JPanel(new BorderLayout());
+        this.campoPesquisa = new JTextField(20);
+        this.campoPesquisa.setPreferredSize(new Dimension(300, 30));
+        painelPesquisa.add(this.campoPesquisa, BorderLayout.CENTER);
+
+        this.botaoPesquisar = new JButton("Pesquisar");
+        painelPesquisa.add(this.botaoPesquisar, BorderLayout.EAST);
 
         DefaultTableModel tableModel = new DefaultTableModel() {
             @Override
@@ -40,18 +54,24 @@ public class ListarFuncionariosView extends BaseView {
         this.tabelaModel = tableModel;
 
         JScrollPane scrollPane = new JScrollPane(tabela);
-        this.add(scrollPane, BorderLayout.CENTER);
-
+        
         JPanel botoesPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         
         this.botaoEditar = new JButton("Editar");
-        this.botaoVisualizar = new JButton("Ver");
-        this.botaoExcluir = new JButton("Excluir");
+        this.botaoEditar.setPreferredSize(new Dimension(100, 30));
 
+        this.botaoVisualizar = new JButton("Ver");
+        this.botaoVisualizar.setPreferredSize(new Dimension(100, 30));
+
+        this.botaoExcluir = new JButton("Excluir");
+        this.botaoExcluir.setPreferredSize(new Dimension(100, 30));
+        
         botoesPanel.add(botaoEditar);
         botoesPanel.add(botaoVisualizar);
         botoesPanel.add(botaoExcluir);
-
+        
+        this.add(painelPesquisa, BorderLayout.NORTH);
+        this.add(scrollPane, BorderLayout.CENTER);
         this.add(botoesPanel, BorderLayout.SOUTH);
     }
 
@@ -77,5 +97,13 @@ public class ListarFuncionariosView extends BaseView {
 
     public JButton getBotaoVisualizar() {
         return this.botaoVisualizar;
+    }
+
+    public JTextField getCampoPesquisa() {
+        return this.campoPesquisa;
+    }
+
+    public JButton getBotaoPesquisar() {
+        return this.botaoPesquisar;
     }
 }
